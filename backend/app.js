f2b506ca10486d05a913c810a1808a7cfea4c9b1
belhazzh5 <belhassenhaboubi@gmail.com>
2026-01-30 19:08:58 +0100

    add frontend endpoing

--- backend/app.js
+++ backend/app.js
@@ -5,26 +5,28 @@ import userRouter from "./routes/userRoute.js";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
 
-// app config
 const app = express();
+app.disable("x-powered-by"); // hide Express version
 
-//middlewares
+const allowedOrigins = ["http://localhost:3000", "http://192.168.124.254:30080"];
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS not allowed"));
+    }
+  }
+}));
+
 app.use(express.json());
-app.use(cors());
-
-// api endpoints
 app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"));
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 
-app.get("/", (req, res) => {
-  res.send("API Working");
-});
-
-app.get("/health", (req, res) => {
-  res.json({ status: "healthy" });
-});
+app.get("/", (req, res) => res.send("API Working"));
 
 export default app;
+
